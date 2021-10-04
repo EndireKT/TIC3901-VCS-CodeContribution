7a5ecfce866af5182c20e2aa667a49bb3031fd40
EndireKT <72508923+EndireKT@users.noreply.github.com>
2021-10-04 12:03:44 +0800

    v2-3: Database: Insert method

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -1,21 +1,43 @@
 public class Message {
     public static void msgWelcome() {
         System.out.println("Hello! Welcome to TIC3901 Project");
-        System.out.println("_________________________________");
+        System.out.println("----------------------------------");
+    }
+
+    public static void msgConnection() {
+        System.out.println("Connected!");
+        System.out.println("----------------------------------");
     }
 
     public static void msgDBInsert() {
         System.out.println("    Data has been inserted!");
-        System.out.println("_________________________________");
+        System.out.println("----------------------------------");
+    }
+
+    public static void msgDBPrUserRow(String lastName, String firstName, String id) { // temporary for database tutorial
+        System.out.println("Query for entire row:");
+        System.out.println("    lastName: " + lastName);
+        System.out.println("    firstName: " + firstName);
+        System.out.println("    id: " + id);
+        System.out.println("----------------------------------");
+    }
+
+    public static void msgDBPrUserRow(String selectFrom, String fromTable, String where, String whereEqualsTo,
+            String result) {
+        System.out.println("Query for specified row:");
+        System.out.println("    << SELECT " + selectFrom + " FROM " + fromTable);
+        System.out.println("        WHERE " + where + " = " + whereEqualsTo + " >>");
+        System.out.println("    result: " + result);
+        System.out.println("----------------------------------");
     }
 
     public static void msgError(Exception e) {
         System.out.println(e.toString());
-        System.out.println("_________________________________");
+        System.out.println("----------------------------------");
     }
 
     public static void msgBye() {
         System.out.println("Bye!");
-        System.out.println("_________________________________");
+        System.out.println("----------------------------------");
     }
 }
