1652ced53a0c4ce87f10286681936d5b93165e41
ksw95 <kshaowei95@gmail.com>
2021-10-07 02:59:14 +0800

    Created GetFileName method.

--- src/main/java/FileController.java
+++ src/main/java/FileController.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
+
 public class FileController {
 
+    public static String GetFileName() {
+        System.out.println("Please input txt file name:");
+        Scanner in = new Scanner(System.in);
+        String FileName = in.nextLine() + ".txt";
+        return FileName;
+    }
     
 }
