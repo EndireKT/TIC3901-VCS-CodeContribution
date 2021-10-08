4b0534239422023999c08394fbeb7190ce75d226
EndireKT <72508923+EndireKT@users.noreply.github.com>
2021-10-08 17:58:05 +0800

    v3-1: Presentation Materials

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -33,34 +33,7 @@ public class Message {
     }
 
     // Database Related
-/*
-    public static void msgConnection() {
-        System.out.println("Connected!");
-        System.out.println("----------------------------------");
-    }
 
-    public static void msgDBInsert() {
-        System.out.println("    Data has been inserted!");
-        System.out.println("----------------------------------");
-    }
-
-    public static void msgDBPrUserRow(String lastName, String firstName, String id) { // temporary for database tutorial
-        System.out.println("    Query for entire row:");
-        System.out.println("    lastName: " + lastName);
-        System.out.println("    firstName: " + firstName);
-        System.out.println("    id: " + id);
-        System.out.println("----------------------------------");
-    }
-
-    public static void msgDBPrUserRow(String selectFrom, String fromTable, String where, String whereEqualsTo,
-            String result) {
-        System.out.println("    Query for specified row:");
-        System.out.println("    << SELECT " + selectFrom + " FROM " + fromTable);
-        System.out.println("        WHERE " + where + " = " + whereEqualsTo + " >>");
-        System.out.println("    result: " + result);
-        System.out.println("----------------------------------");
-    }
-*/
     public static void msgDBUpdate() {
         System.out.println("    Data has been updated!");
         System.out.println("----------------------------------");
